c2934654396b63d50818b577eefacbcf4b9204e0
hwang104 <hwang104@illinois.edu>
2024-02-21 10:09:03 -0600

    syntax

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,31 @@
-# @summary A short summary of the purpose of this class
+# @summary Creates the memtest script file, and the performance table file that it will reference.
 #
-# A description of what this class does
+# @param performance_table
+#   A hash of regex representing a range of nodes and their expected STREAM triad performance values
+# 
+# @param stream_path
+#   The path of the STREAM binary if not the default location
 #
 # @example
 #   include profile_memtest
 class profile_memtest {
-  include profile_gpu::performance_table
-  include profile_gpu::stream_path
+
+  Hash   $performance_table
+  String $stream_path
+
+# table
+#  file {
+#    ensure  => 'file',
+#    mode    => '644',
+#    content => template('profile_memtest/memtest.table.epp'),
+#  }
+
+# script
+  file { '/root/scripts/memtest_script':
+    ensure  => file,
+    mode    => '755',
+#    content => template('profile_memtest/memtest.script.epp'),
+    content => "${stream_path}",  
+  }
+
 }
